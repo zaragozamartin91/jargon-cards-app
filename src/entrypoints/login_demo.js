0f803e9da58bc24184ba1a048e70c1ff2b350eb6
zaragozamartin91 <zaragozamartin91@gmail.com>
2023-07-13 19:00:43 +0200

    Removing unused code

--- src/entrypoints/login_demo.js
+++ src/entrypoints/login_demo.js
@@ -5,42 +5,6 @@ import LoginDemo from '../components/LoginDemo'
 
 const container = document.getElementById('app')
 
-function asdasd() {
-    const firebaseFacade = FirebaseFacade.getDefault()
-    const firebaseAuth = firebaseFacade.getAuth()
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        alert('You have submitted the form.')
-
-        signInWithEmailAndPassword(firebaseAuth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-            });
-    }
-
-    return (
-        <div className="wrapper">
-            <h1>How About Them Apples</h1>
-            <form onSubmit={handleSubmit}>
-                <fieldset>
-                    <label>
-                        <p>Name</p>
-                        <input name="name" />
-                    </label>
-                </fieldset>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
-}
-
 
 // Create a root.
 const root = ReactDOMClient.createRoot(container)
